0dfac531c464c10c89f04aa43081c117a03b1b16
vishwani123patlan <vishwani123patlan@gmail.com>
2022-09-26 17:00:54 +0530

    changes

--- app/models/super_admin.rb
+++ app/models/super_admin.rb
@@ -2,7 +2,7 @@ class SuperAdmin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   def name
     "#{first_name} #{last_name}"
